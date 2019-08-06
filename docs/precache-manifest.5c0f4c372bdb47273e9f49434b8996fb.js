c24d19c83f33f40393c47825f542cf91fe90f7da
Oleh Sudavnyi <osudav@dp518.softservecom.com>
2019-08-06 21:39:48 +0300

    build

---
+++ docs/precache-manifest.5c0f4c372bdb47273e9f49434b8996fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "419ab0c3b75803ef68b0f1994a8eda2d",
+    "url": "/taxistat/index.html"
+  },
+  {
+    "revision": "b4343129bc15acc701d6",
+    "url": "/taxistat/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e65084bff1558a33837c",
+    "url": "/taxistat/static/js/2.b8b680ad.chunk.js"
+  },
+  {
+    "revision": "b4343129bc15acc701d6",
+    "url": "/taxistat/static/js/main.e82ab989.chunk.js"
+  },
+  {
+    "revision": "441d70845b83bd49b1cf",
+    "url": "/taxistat/static/js/runtime~main.850b3200.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c9f0284ddf20222c770818ba574b43e4.js"
+  "/taxistat/precache-manifest.5c0f4c372bdb47273e9f49434b8996fb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/taxistat/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
