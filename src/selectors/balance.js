46f409e9ab57ff80a421f0fc82e61ff391a15a28
Oleg Sudavniy <acestudiooleg@gmail.com>
2019-10-04 17:58:45 +0300

    fixed today balance

--- src/selectors/balance.js
+++ src/selectors/balance.js
@@ -2,17 +2,19 @@ import moment from 'moment';
 import { createSelector } from 'reselect';
 import { getRides } from '../reducers/rides';
 import { getExpenses } from '../reducers/expenses';
+import { getStatisticts } from '../reducers/statistics';
 
 export const getBalance = createSelector(
   getRides,
   getExpenses,
-  ({ list: rides }, { list: expensesList }) => {
+  getStatisticts,
+  ({ list: rides }, { list: expensesList }, { currentDate }) => {
     const earn = (rides || []).reduce((acc, el) => acc + Number(el.profit), 0);
     const distance = (rides || []).reduce((acc, el) => acc + Number(el.distance), 0);
     const expenses = (expensesList || []).reduce((acc, el) => acc + Number(el.value), 0);
     const balance = earn - expenses;
     const earnToday = (rides || [])
-      .filter(el => moment(el.timestamp).isSame(moment(), 'day'))
+      .filter(el => moment(el.timestamp).isSame(moment(currentDate), 'day'))
       .reduce((acc, el) => acc + Number(el.profit), 0);
     return {
       earn,
